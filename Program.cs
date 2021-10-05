03b96a23aa2f035a1be02933d79a75f7dbf9d71a
alan-0001 <88444410+alan-0001@users.noreply.github.com>
2021-10-05 00:25:59 -0300

    Desafio_03
    Imprimir mensagema quantidade de Alunos
    Qual o nome do Aluno
    Qual a nota do Aluno
    Aluno com maior nota

--- Program.cs
+++ Program.cs
@@ -6,80 +6,25 @@ namespace Desafio_01
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("UBUNTU 002 - STARTS...");
+            Console.WriteLine("UBUNTU 003 - STARTS...");
 
-            string nome1 = "Ubuntu Silva";
-            int idade1 = 33;
-            double peso1 = 88.50;
-            double altura1 = 1.65;
-            bool ehDev1 = true;
-            //var imc1 = 32.50;
-            
-            string nome2 = "Ubuntu Bruno";
-            int idade2 = 26;
-            double peso2 = 84.50;
-            double altura2 = 2.10;
-            bool ehDev2 = true;
-            //var imc2 = 19.16;
-            
-            string nome3 = "Ubuntu Silva";
-            int idade3 = 85;
-            double peso3 = 66.50;
-            double altura3 = 1.40;
-            bool ehDev3 = false;
-            //var imc3 = 33.92;
+            Console.WriteLine("Digite a quantidade de Alunos");
+            int Quantidade = int.Parse(Console.ReadLine());
 
-            //T1 - Soma das idades dos Ubuntus
-            Console.WriteLine(idade1 + idade2+ idade3);
-            
-            //T2 - Agrupar o nome dos Ubuntus
-            Console.WriteLine("{0} {1} {2}", nome1, nome2, nome3);
+            Console.WriteLine("Qual o nome do Aluno?");
+            string nome = Console.ReadLine();
 
-            //T3 - Média IMC dos Ubuntus
-            double imc = peso1 / (altura1 * altura1);
-            var imc2 = peso2 / (altura2 * altura2);
-            var imc3 = peso3 / (altura3 * altura3);
-            var Média = imc + imc2 + imc3;
+            Console.WriteLine("Qual a nota do Aluno?");
+            int Nota = int.Parse(Console.ReadLine());
 
-            Console.WriteLine($"IMC nome1 é {imc}.");
-            Console.WriteLine($"IMC nome2 é {imc2}.");
-            Console.WriteLine($"IMC nome3 é {imc3}.");
-            Console.WriteLine($"A média é: {Média.ToString("F")}");
-
-            //T4 - Quantos são Devs
-            if (ehDev1 == true)
+            if (Nota >= 7)
             {
-                Console.WriteLine("Sou Dev");
+                Console.WriteLine($"Aluno com maior nota:");
+                Console.WriteLine(nome);
             }
             else
             {
-                Console.WriteLine("Não sou Dev");
-            }
-            if (ehDev2 == true)
-            {
-                Console.WriteLine("Sou Dev");
-            }
-            else
-            {
-                Console.WriteLine("Não soe Dev");
-            }
-            if (ehDev3 == true)
-            {
-                Console.WriteLine("Sou Dev");
-            }
-            else
-            {
-                Console.WriteLine("Não sou Dev");
-            }
-
-            //T5 - Somente Umbuntus com Silva
-            if (string.Equals(nome1, nome3))
-            {
-                Console.WriteLine($"{nome1} e {nome3} ");
-            }
-            else
-            {
-                Console.WriteLine($"{nome2}");
+                Console.WriteLine("Aluno com maior nota é Pedro");
             }
 
         }
